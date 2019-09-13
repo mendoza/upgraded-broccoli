15f6153b8cfb7cc7064ef30285eff44dab567f14
David Mendoza <dmendoza99@unitec.ede>
2019-09-13 13:12:40 -0600

    souts

--- src/upgraded/broccoli/WordCount.java
+++ src/upgraded/broccoli/WordCount.java
@@ -67,7 +67,7 @@ public class WordCount {
                 StringTokenizer itr = new StringTokenizer(review);
                 while (itr.hasMoreTokens()) {
                     word.set(itr.nextToken());
-                    System.out.println(word.toString());
+                    System.out.println(word.toString() + !contains(stopwords, word.toString()));
                     if (!contains(stopwords, word.toString())) {
                         context.write(word, one);
                     }
