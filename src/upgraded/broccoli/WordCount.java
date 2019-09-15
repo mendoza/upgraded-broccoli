c0df2894db0f2f0f7c251ce234e8917ffbe508e6
Diego <dam_900@hotmail.com>
2019-09-15 14:17:29 -0600

    push

--- src/upgraded/broccoli/WordCount.java
+++ src/upgraded/broccoli/WordCount.java
@@ -65,9 +65,14 @@ public class WordCount {
                     if (i + 1 < words.length) {
                         palabras += " " + words[i + 1];
                     }
+                    System.out.println("aqui esta palabras :" + palabras );
+                    
                     word.set(palabras);
                     String[] TwoWords = palabras.split(" ");
-                    if (!list.contains(TwoWords[0]) && ! list.contains(TwoWords[1])) {
+                    for (String TwoWord : TwoWords) {
+                        System.out.println("esto es partes de two words"+TwoWords[i]);
+                    }
+                    if (!list.contains(TwoWords[0]) && !list.contains(TwoWords[1])) {
                         context.write(word, one);
                     }
                 }
