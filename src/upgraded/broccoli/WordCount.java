6819d9bcb0e8efa8918934eb706686d314e04100
Diego <dam_900@hotmail.com>
2019-09-15 15:36:38 -0600

    Cambio a dos ifs

--- src/upgraded/broccoli/WordCount.java
+++ src/upgraded/broccoli/WordCount.java
@@ -65,14 +65,9 @@ public class WordCount {
                     if (i + 1 < words.length) {
                         palabras += " " + words[i + 1];
                     }
-                    System.out.println("aqui esta palabras: " + palabras);
-
                     word.set(palabras);
                     String[] TwoWords = palabras.split(" ");
-                    for (String TwoWord : TwoWords) {
-                        System.out.println("esto es partes de two words: " + TwoWord);
-                    }
-                    if (TwoWords.length > 1 && TwoWords.length < 3) {
+                    if (TwoWords.length == 2) {
                         if (!list.contains(TwoWords[0]) && !list.contains(TwoWords[1])) {
                             context.write(word, one);
                         }
