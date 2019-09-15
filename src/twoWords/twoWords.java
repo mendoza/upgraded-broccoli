dc6f30056489a576dd7951f2b5a922fd5b1fce24
Diego <dam_900@hotmail.com>
2019-09-15 17:58:30 -0600

    makefiles

--- src/twoWords/twoWords.java
+++ src/twoWords/twoWords.java
@@ -23,7 +23,6 @@ public class twoWords {
         @Override
         public void map(Object key, Text value, Reducer.Context context
         ) throws IOException, InterruptedException {
-            boolean oneWord = true;
             String linea = value.toString();
             String[] stopwords = new String[]{"a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along",
                 "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything",
@@ -50,6 +49,7 @@ public class twoWords {
             String regex = "[.,&()\\[\\]{}#0-9!?\\\\\\'\\\"-*\\~_;\\+\\-@\\^|\\:\\/\\`=<>]";
             String review = splitLinea[1].replaceAll(regex, " ");
             review = review.replaceAll("\\s+", " ");
+            StringTokenizer itr = new StringTokenizer(review.toLowerCase());
             String[] words = review.toLowerCase().split(" ");
             for (int i = 0; i < words.length; i++) {
                 String palabras = words[i];
@@ -63,41 +63,39 @@ public class twoWords {
                         context.write(word, one);
                     }
                 }
-
             }
         }
-    }
 
-    public static class IntSumReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public static class IntSumReducer
+                extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-        private IntWritable result = new IntWritable();
+            private IntWritable result = new IntWritable();
 
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values,
-                Reducer.Context context
-        ) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+            @Override
+            public void reduce(Text key, Iterable<IntWritable> values,
+                    Reducer.Context context
+            ) throws IOException, InterruptedException {
+                int sum = 0;
+                for (IntWritable val : values) {
+                    sum += val.get();
+                }
+                result.set(sum);
+                context.write(key, result);
             }
-            result.set(sum);
-            context.write(key, result);
+        }
+
+        public static void main(String[] args) throws Exception {
+            Configuration conf = new Configuration();
+            Job job = Job.getInstance(conf, "word count");
+            job.getConfiguration().setStrings("mapreduce.reduce.shuffle.memory.limit.percent", "0.15");
+            job.setJarByClass(twoWords.class);
+            job.setMapperClass(TokenizerMapper.class);
+            job.setCombinerClass(IntSumReducer.class);
+            job.setReducerClass(IntSumReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.getConfiguration().setStrings("mapreduce.reduce.shuffle.memory.limit.percent", "0.15");
-        job.setJarByClass(oneWord.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
-}
